db01f217a600f8f8868df5b93534542851ba0131
Rajeev Kotha <kotha.rajeev@gmail.com>
2023-05-30 19:40:46 -0600

    First commit

---
+++ src/main/java/org/comit/course_01_variables/Variables.java
@@ -0,0 +1,19 @@
+package org.comit.course_01_variables;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+		
+		/*
+		 * Comments:
+		 * 1. Variable declaration
+		 * 2. Variable initialization
+		 */
+		int num = 5;
+		
+		System.out.println("The value is: " + num);
+
+	}
+
+}
